9190f83ff4ca32249a4a00873265fcec2eec2bff
Federico Orlandau <federico.orlandau@gmail.com>
2021-09-07 18:26:27 +0200

    Fix import

--- src/api/contentful.ts
+++ src/api/contentful.ts
@@ -1,4 +1,4 @@
-import * as contentful from 'contentful';
+import contentful from 'contentful';
 import type { PageFields } from 'src/types';
 import type { Entry } from 'contentful';
 
@@ -9,8 +9,6 @@ const client = contentful.createClient({
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
   accessToken: import.meta.env.VITE_CONTENTFUL_TOKEN as string
 });
-
-
 
 export const getPageBySlug = async (slug: string)
     : Promise<Entry<PageFields>> => {
