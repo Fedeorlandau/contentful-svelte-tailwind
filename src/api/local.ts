63c0be5f5d873eafe10052475e999e22a5c1b1e9
Federico Orlandau <federico.orlandau@gmail.com>
2021-09-08 16:04:34 +0200

    Fix api base url

--- src/api/local.ts
+++ src/api/local.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const local = axios.create({
-    baseURL: import.meta.env.DEV ? 'http://localhost:3000' : import.meta.env.BASE_URL as string
-})
+const local = import.meta.env.DEV ? axios.create({
+    baseURL: 'http://localhost:3000'
+}) : axios
 
 export default local;
