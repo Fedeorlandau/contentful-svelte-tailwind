310f804a3f6b49bd51eb24ec7069b255aaffcf98
Federico Orlandau <federico.orlandau@gmail.com>
2021-09-10 13:05:44 +0200

    Add logo

--- src/components/Navbar/Navbar.svelte
+++ src/components/Navbar/Navbar.svelte
@@ -13,8 +13,8 @@
 					<span class="sr-only">Workflow</span>
 					<img
 						class="h-8 w-auto sm:h-10"
-						src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-						alt=""
+						src="/logo.svg"
+						alt="Logo"
 					/>
 				</a>
 			</div>
